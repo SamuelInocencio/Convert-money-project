cbac5f08b1f6fdc0c9852e9a8fa0405a6b944bd5
SamuelInocencio <samuelferreirainocencio@gmail.com>
2025-01-08 17:22:48 -0300

    projeto concluido

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const convertButton = document.getElementById('convert-button');
+const currencySelect = document.getElementById('currency-select');
+const outputCurrencyFlag = document.getElementById('output-currency-flag');
+const outputCurrencyTitle = document.getElementById('output-currency-title');
+
 
 
 
@@ -6,13 +10,13 @@ function convertValues() {
     const inputCurrencyValue = document.getElementById('input-value').value;
     const inputCurrency = document.getElementById('input-currency');
     const outputCurrency = document.getElementById('output-currency');
-    const currencySelect = document.getElementById('currency-select').value;
+
 
 
     const dolarToday = 6.2;
     const euroToday = 7;
 
-    switch (currencySelect) {
+    switch (currencySelect.value) {
         case 'dolar':
             outputCurrency.innerHTML = new Intl.NumberFormat('en-US', {
                 style: 'currency',
@@ -38,10 +42,25 @@ function convertValues() {
         style: 'currency',
         currency: 'BRL'
     }).format(inputCurrencyValue);
-
-
-
-
 }
 
+function changeCurrency() {
+    switch (currencySelect.value) {
+        case 'dolar':
+            outputCurrencyFlag.src = "./assets/dolar.png";
+            outputCurrencyTitle.innerHTML = "Dolar Americano";
+            break;
+        case 'euro':
+            outputCurrencyFlag.src = "./assets/euro.png";
+            outputCurrencyTitle.innerHTML = "Euro";
+            break;
+        default:
+            // Adicione o código para o caso padrão
+            break;
+    }
+    convertValues();
+}
+
+
+currencySelect.addEventListener("change", changeCurrency);
 convertButton.addEventListener('click', convertValues);
